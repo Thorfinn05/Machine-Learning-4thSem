9584f01eff4f9287856fd830568df395dde83332
Rudranil Das <rudranild04@gmail.com>
2025-08-05 21:14:25 +0530

    Enhance weather data analysis by adding unique outlook categorization and printing subsets

--- gain.py
+++ gain.py
@@ -3,8 +3,13 @@ import pandas as pd
 
 data = pd.read_csv("C:\\Users\\User\\OneDrive\\Documents\\Weather.csv")
 print(data)
+
 Sunny = len(data[data['Outlook'] == 'Sunny'])
 Sunny_Y = len(data[(data['Outlook'] == 'Sunny') & (data['Play Tennis'] == 'Yes')])
 Sunny_N = len(data[(data['Outlook'] == 'Sunny') & (data['Play Tennis'] == 'No')])
 
 print(Sunny, Sunny_Y, Sunny_N)
+cate= data['Outlook'].unique()
+for cat in cate:
+    sub = data[data['Outlook'] == cat]
+    print(sub)
