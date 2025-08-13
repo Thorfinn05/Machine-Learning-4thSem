dc9ae754a49f9d88ebc760e094928299d28bedd7
Rudranil Das <rudranild04@gmail.com>
2025-08-13 11:50:32 +0530

    Add input handling and feature value printing for sentiment analysis

--- sentiment.py
+++ sentiment.py
@@ -2,3 +2,13 @@ positive_words = {"good", "great", "amazing", "enjoyable", "nice", "fantastic", 
 negative_words = {"bad", "worst", "boring", "terrible", "awful", "poor", "hate", "2nd grade", "waste", "disappointing"}
 pronouns = {"I", "me", "my", "you", "your"}
 
+review_text = input ("Enter a review: ")
+x1, x2, x3, x4, x5, x6 = analyze_review(review_text)
+
+print("\n Feature values: ");
+print("\n x1(Positive words): ",x1, 
+      "\n x2 (Negative words): ",x2, 
+      "\n x3 (Contains 'no'): ",x3, 
+      "\n x4 (Prononun Counts): ",x4, 
+      "\n x5 (Exclamation marks): ",x5, 
+      "\n x6 Total words): ",x6)
