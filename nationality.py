f1a7135dd98fcee95cddee80e0ea4782838587c1
Rudranil Das <rudranild04@gmail.com>
2025-08-13 11:17:41 +0530

    Add initial implementation of nationality data loading and visualization

---
+++ nationality.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+data = pd.read_csv("C:\\Users\\User\\OneDrive\\Documents\\Nationality.csv")
+print(data)
