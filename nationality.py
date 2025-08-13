be03e11e741f1c2081bf8bc647084b57864e12b0
Rudranil Das <rudranild04@gmail.com>
2025-08-13 11:23:23 +0530

    Implement decision tree classifier and visualization for nationality data

--- nationality.py
+++ nationality.py
@@ -4,3 +4,15 @@ from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 data = pd.read_csv("C:\\Users\\User\\OneDrive\\Documents\\Nationality.csv")
 print(data)
+d = { 'UK':0, 'USA':1, 'N':2 }
+data['Nationality'] = data['Nationality'].map(d)
+d = { 'YES':1, 'NO':0 }
+data['Go'] = data['Go'].map(d)
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+X = data[features]
+Y = data['Go']
+dtree = DecisionTreeClassifier()
+dtree.fit(X,Y)
+plt.figure(figsize=(10,6))
+tree.plot_tree(dtree, feature_names=features, filled = True)
+plt.show()
