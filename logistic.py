d87f07a67087b3ab5919f542c7a0b6f1858033a7
Rudranil Das <rudranild04@gmail.com>
2025-08-23 20:58:01 +0530

    Fix punctuation in movie review and update logistic.py to include word count functionality

--- logistic.py
+++ logistic.py
@@ -4,3 +4,7 @@ with open ("D:\Github\Machine-Learning-4thSem\Movie Review.txt") as file:
 review = review.lower()
 words = review.split()
 print(words)
+x6 = 0
+for word in words:
+    x6 += 1
+print(x6)
