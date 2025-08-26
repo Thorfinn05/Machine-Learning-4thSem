8891d12b530ff01b531f4fa0198821d966a472a7
Rudranil Das <rudranild04@gmail.com>
2025-08-26 15:43:01 +0530

    Refactor sentiment analysis feature extraction: streamline word counting and remove unnecessary variables

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -12,59 +12,40 @@ with open ("D:\Github\Machine-Learning-4thSem\Movie Review.txt") as file:
     review = file.read()
     print(review)
 review = review.lower()
-words = review.split()
-print(words)
-modified_review = words
+words_modified = review.split()
+print(words_modified)
 
 punctuations = [",", ".", "?"]
 positive_words = ["sure", "enjoyable", "great", "nice", "good", "fantastic", "wow", "charming"]
 negative_words = ["second-grade", "hokey", "bad", "boring", "break-up", "worthless"]
 pronouns = ['i', "me", "you", "they", "we", "he", "she", "them"]
 
-features = []
-x1 =0
-x2 = 0
-x3 = 0
-x4 = 0
-x5 = 0
-x6 = 0
+features = [0,0,0,0,0,0]
 
-for word in words:
-    x6 += 1
-print(x6)
-
-for word in words:
+for word in words_modified:
     if word in punctuations:
-        words.remove(word)
-print(modified_review)
+        words_modified.remove(word)
 
-for word in modified_review:
+for word in words_modified:
+    features[5] += 1
     if word in positive_words:
-        x1 += 1
+        features[0] += 1
     elif word in negative_words:
-        x2 += 1
+        features[1] += 1
     elif word in pronouns:
-        x4 += 1
+        features[3] += 1
     elif (word == "no"):
-        x3 += 1
+        features[2] += 1
     elif (word == "!"):
-        x5 += 1
-x6 = math.log(x6)
+        features[4] += 1
+features[5] = math.log(features[5])
 
-print("Positive words: ", x1)
-print("Negative words: ", x2)
-print("Number of no: ", x3)
-print("Number of Prononuns: ", x4)
-print("Number of Excalimation: ", x5)
-print("Number of words(log): ", x6)
-
-features.append(x1)
-features.append(x2)
-features.append(x3)
-features.append(x4)
-features.append(x5)
-features.append(x6)
-print("Features: \n", features)
+print("Positive words: ", features[0])
+print("Negative words: ", features[1])
+print("Number of no: ", features[2])
+print("Number of Prononuns: ", features[3])
+print("Number of Excalimation: ", features[4])
+print("Number of words(log): ", features[5])
 
 weights = [2.5, -5.0, -1.2, 0.5, 2.0, 0.7]
 bias = 0.1
